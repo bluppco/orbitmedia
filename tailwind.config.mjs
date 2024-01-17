65e29511af6c05db1177ac651720ab34c20e5bb8
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-01-17 18:52:56 +0530

    feat: created header component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,11 +3,19 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+
 			fontFamily: {
 
 				"proxima_nova_condensed": [ "proxima-nova-condensed", "sans-serif" ],
 
 			},
+
+			colors: {
+
+				"om_orange": "#d85b41",
+
+			}
+
 		},
 	},
 	plugins: [],
